6efdcb751cf7c0e107058445b3029dbc54a8283d
Aman Gupta <aman@keio.jp>
2020-03-15 23:28:21 +0900

    adding the DB model for questions

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -20,9 +20,11 @@ connection.once('open', () => {
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
+const questionsRouter = require('./routes/questions');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+app.use('/questions', questionsRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
